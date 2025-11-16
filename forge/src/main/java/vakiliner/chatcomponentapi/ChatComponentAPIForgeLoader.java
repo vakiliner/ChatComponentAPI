ce2f243d8a972e713f07a406fe49d544a340594b
VAKiliner <119078586+vakiliner@users.noreply.github.com>
2025-11-16 14:41:57 +0300

    Оставить только base

--- forge/src/main/java/vakiliner/chatcomponentapi/ChatComponentAPIForgeLoader.java
+++
@@ -1,9 +0,0 @@
-package vakiliner.chatcomponentapi;
-
-import net.minecraftforge.fml.common.Mod;
-import vakiliner.chatcomponentapi.forge.ForgeParser;
-
-@Mod("chatcomponentapi")
-public class ChatComponentAPIForgeLoader {
-	public static final ForgeParser PARSER = new ForgeParser();
-}
