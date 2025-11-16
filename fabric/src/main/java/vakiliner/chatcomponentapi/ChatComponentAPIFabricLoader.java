eff9fd140921218262fd1f0c37cf65239653269f
VAKiliner <119078586+vakiliner@users.noreply.github.com>
2025-11-16 14:57:40 +0300

    chore: Добавить Fabric

---
+++ fabric/src/main/java/vakiliner/chatcomponentapi/ChatComponentAPIFabricLoader.java
@@ -0,0 +1,11 @@
+package vakiliner.chatcomponentapi;
+
+import net.fabricmc.api.ModInitializer;
+import vakiliner.chatcomponentapi.fabric.FabricParser;
+
+public class ChatComponentAPIFabricLoader implements ModInitializer {
+	public static final FabricParser PARSER = new FabricParser();
+
+	public void onInitialize() {
+	}
+}
