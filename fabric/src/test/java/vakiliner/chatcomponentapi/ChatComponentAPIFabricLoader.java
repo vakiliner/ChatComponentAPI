741fd7eb70e594d7e4bdafbeca55ee81b9fb0101
VAKiliner <119078586+vakiliner@users.noreply.github.com>
2025-11-17 21:52:54 +0300

    test: Add tests

--- fabric/src/test/java/vakiliner/chatcomponentapi/ChatComponentAPIFabricLoader.java
+++ fabric/src/test/java/vakiliner/chatcomponentapi/ChatComponentAPIFabricLoader.java
@@ -1,14 +1,32 @@
 package vakiliner.chatcomponentapi;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 import java.util.function.BooleanSupplier;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.network.chat.ChatType;
+import net.minecraft.network.chat.ClickEvent;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.HoverEvent;
+import net.minecraft.network.chat.SelectorComponent;
 import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.network.chat.ClickEvent.Action;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import vakiliner.chatcomponentapi.common.ChatId;
+import vakiliner.chatcomponentapi.common.ChatMessageType;
+import vakiliner.chatcomponentapi.component.ChatClickEvent;
 import vakiliner.chatcomponentapi.component.ChatComponent;
+import vakiliner.chatcomponentapi.component.ChatComponentWithLegacyText;
+import vakiliner.chatcomponentapi.component.ChatHoverEvent;
+import vakiliner.chatcomponentapi.component.ChatTextComponent;
 import vakiliner.chatcomponentapi.fabric.FabricParser;
 
 public class ChatComponentAPIFabricLoader implements ModInitializer {
@@ -19,36 +37,94 @@ public class ChatComponentAPIFabricLoader implements ModInitializer {
 
 	public void onInitialize() {
 		LOGGER.info("Tests started");
-		test(() -> {
+		test("Parse text component", () -> {
 			Component input = new TextComponent("123");
-			ChatComponent output = FabricParser.fabric(input);
-			Component result = FabricParser.fabric(output);
-			return input.equals(result);
+			ChatComponent test = FabricParser.fabric(input);
+			Component output = FabricParser.fabric(test);
+			return input.equals(output);
+		});
+		test("Parse translatable component", () -> {
+			Component input = new TranslatableComponent("123");
+			ChatComponent test = FabricParser.fabric(input);
+			Component output = FabricParser.fabric(test);
+			return input.equals(output);
+		});
+		test("Parse selector component", () -> {
+			Component input = new SelectorComponent("123");
+			ChatComponent test = FabricParser.fabric(input);
+			Component output = FabricParser.fabric(test);
+			return input.equals(output);
+		});
+		test("Parse click event", () -> {
+			for (Action action : Action.values()) {
+				ClickEvent input = new ClickEvent(action, "/test");
+				ChatClickEvent test = FabricParser.fabric(input);
+				ClickEvent output = FabricParser.fabric(test);
+				if (!input.equals(output)) {
+					return false;
+				}
+			}
+			return true;
+		});
+		test("Parse hover event", () -> {
+			HoverEvent[] input = { new HoverEvent(HoverEvent.Action.SHOW_TEXT, new TextComponent("123")), new HoverEvent(HoverEvent.Action.SHOW_ENTITY, new HoverEvent.EntityTooltipInfo(EntityType.CREEPER, UUID.randomUUID(), new TextComponent("Hello"))), new HoverEvent(HoverEvent.Action.SHOW_ITEM, new HoverEvent.ItemStackInfo(new ItemStack(Items.DIRT, 15))) };
+			HoverEvent[] output = new HoverEvent[input.length];
+			for (int i = 0; i < input.length; i++) {
+				ChatHoverEvent<?> test = FabricParser.fabric(input[i]);
+				output[i] = FabricParser.fabric(test);
+			}
+			return Arrays.equals(input, output);
+		});
+		test("Parse resource location", () -> {
+			ResourceLocation input = new ResourceLocation("chatcomponentapi", "id");
+			ChatId test = FabricParser.fabric(input);
+			ResourceLocation output = FabricParser.fabric(test);
+			return input.equals(output);
+		});
+		test("Parse chat type", () -> {
+			ChatType[] input = { ChatType.CHAT, ChatType.SYSTEM };
+			ChatType[] output = new ChatType[input.length];
+			for (int i = 0; i < input.length; i++) {
+				ChatMessageType test = FabricParser.fabric(input[i]);
+				output[i] = FabricParser.fabric(test);
+			}
+			return Arrays.equals(input, output);
+		});
+		test("ChatComponentWithLegacyText", () -> {
+			String legacyText = "legacy";
+			ChatComponent chatComponent = new ChatTextComponent("123");
+			ChatComponentWithLegacyText chatComponentWithLegacyText = chatComponent.withLegacyText(legacyText);
+			return chatComponentWithLegacyText.toLegacyText().equals(legacyText) && chatComponentWithLegacyText.getComponent() == chatComponent;
 		});
 		LOGGER.info("Fails " + failCount);
 		ERRORS.forEach(Throwable::printStackTrace);
 	}
 
 	@SuppressWarnings("unused")
-	private static boolean test(Runnable runnable) {
-		return test(() -> {
+	private static boolean test(String name, Runnable runnable) {
+		return test(name, () -> {
 			runnable.run();
 			return true;
 		});
 	}
 
-	private static boolean test(BooleanSupplier runnable) {
+	private static boolean test(String name, BooleanSupplier runnable) {
 		final boolean success;
-		try {
-			success = runnable.getAsBoolean();
-		} catch (Throwable err) {
-			ERRORS.add(err);
-			failCount++;
-			return false;
+		final Throwable error;
+		{
+			boolean output = false;
+			Throwable fail = null;
+			try {
+				output = runnable.getAsBoolean();
+			} catch (Throwable err) {
+				fail = err;
+			}
+			success = output;
+			error = fail;
 		}
 		if (!success) {
 			failCount++;
-			ERRORS.add(new RuntimeException("Test failed"));
+			ERRORS.add(new RuntimeException("Test failed: " + name, error));
 		}
 		return success;
 	}
