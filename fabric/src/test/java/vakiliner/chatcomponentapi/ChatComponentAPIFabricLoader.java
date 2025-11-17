3a75c7e79a7e243d9f25061841d25953057399a6
VAKiliner <119078586+vakiliner@users.noreply.github.com>
2025-11-18 02:52:27 +0300

    test: Добавить тесты Component Style (#27)
    * test: Add component style tests
    
    * fix: Тест выявил, что SelectorComponent был изменён в версии 1.17

--- fabric/src/test/java/vakiliner/chatcomponentapi/ChatComponentAPIFabricLoader.java
+++ fabric/src/test/java/vakiliner/chatcomponentapi/ChatComponentAPIFabricLoader.java
@@ -5,12 +5,14 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 import java.util.function.BooleanSupplier;
+import java.util.stream.Collectors;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
+import net.minecraft.ChatFormatting;
 import net.minecraft.commands.CommandSource;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.network.chat.ChatType;
@@ -18,10 +20,11 @@ import net.minecraft.network.chat.ClickEvent;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.HoverEvent;
 import net.minecraft.network.chat.SelectorComponent;
+import net.minecraft.network.chat.Style;
+import net.minecraft.network.chat.TextColor;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.network.chat.ClickEvent.Action;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
@@ -29,6 +32,8 @@ import vakiliner.chatcomponentapi.base.ChatCommandSender;
 import vakiliner.chatcomponentapi.base.ChatPlayer;
 import vakiliner.chatcomponentapi.common.ChatId;
 import vakiliner.chatcomponentapi.common.ChatMessageType;
+import vakiliner.chatcomponentapi.common.ChatNamedColor;
+import vakiliner.chatcomponentapi.common.ChatTextFormat;
 import vakiliner.chatcomponentapi.component.ChatClickEvent;
 import vakiliner.chatcomponentapi.component.ChatComponent;
 import vakiliner.chatcomponentapi.component.ChatComponentWithLegacyText;
@@ -86,9 +91,21 @@ public class ChatComponentAPIFabricLoader implements ModInitializer, CommandRegi
 			Component output = FabricParser.fabric(test);
 			return input.equals(output);
 		});
+		test("Parse text component with style", () -> {
+			Component input = new TextComponent("Hey").withStyle(Style.EMPTY.withBold(true).withItalic(false).withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/chatcomponentapi test")));
+			ChatComponent test = FabricParser.fabric(input);
+			Component output = FabricParser.fabric(test);
+			return input.equals(output);
+		});
+		test("Parse text component with style hover event", () -> {
+			Component input = new TextComponent("Hello").withStyle(Style.EMPTY.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new TextComponent("world"))));
+			ChatComponent test = FabricParser.fabric(input);
+			Component output = FabricParser.fabric(test);
+			return input.equals(output);
+		});
 		test("Parse click event", () -> {
-			for (Action action : Action.values()) {
-				ClickEvent input = new ClickEvent(action, "/test");
+			for (ClickEvent.Action action : ClickEvent.Action.values()) {
+				ClickEvent input = new ClickEvent(action, "/chatcomponentapi test");
 				ChatClickEvent test = FabricParser.fabric(input);
 				ClickEvent output = FabricParser.fabric(test);
 				if (!input.equals(output)) {
@@ -117,6 +134,25 @@ public class ChatComponentAPIFabricLoader implements ModInitializer, CommandRegi
 			ChatType[] output = new ChatType[input.length];
 			for (int i = 0; i < input.length; i++) {
 				ChatMessageType test = FabricParser.fabric(input[i]);
+				output[i] = FabricParser.fabric(test);
+			}
+			return Arrays.equals(input, output);
+		});
+		test("Parse chat formatting", () -> {
+			ChatFormatting[] input = ChatFormatting.values();
+			ChatFormatting[] output = new ChatFormatting[input.length];
+			for (int i = 0; i < input.length; i++) {
+				ChatTextFormat test = FabricParser.fabric(input[i]);
+				output[i] = FabricParser.fabric(test);
+			}
+			return Arrays.equals(input, output);
+		});
+		test("Parse chat colors", () -> {
+			List<TextColor> rawInput = Arrays.asList(ChatFormatting.values()).stream().filter((f) -> !f.isFormat()).map(TextColor::fromLegacyFormat).collect(Collectors.toList());
+			TextColor[] input = rawInput.toArray(new TextColor[rawInput.size()]);
+			TextColor[] output = new TextColor[input.length];
+			for (int i = 0; i < input.length; i++) {
+				ChatNamedColor test = (ChatNamedColor) FabricParser.fabric(input[i]);
 				output[i] = FabricParser.fabric(test);
 			}
 			return Arrays.equals(input, output);
