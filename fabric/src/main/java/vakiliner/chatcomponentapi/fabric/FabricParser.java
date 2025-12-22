36e480e21e8397af19382cbe1edb423451b5f281
VAKiliner <119078586+vakiliner@users.noreply.github.com>
2025-12-22 17:07:31 +0300

    chore: Объединить подпроект в одну ветку (#47)
    * chore: Добавить Fabric
    
    * chore: Update settings.gradle (#8)
    
    * chore: Update gradle.properties (#10)
    
    * chore: Delete .vscode/settings.json (#17)
    
    * refactor: Убрать java.lang.reflect (#14)
    
    * refactor: Убрать java.lang.reflect
    
    * refactor: Удалить ExampleMixin
    
    * chore: Очистить ненужные данные, а также исправить версию Java (#20)
    
    * test: Add fabric tests (#23)
    
    * chore: Update gradle.properties (#12)
    
    * chore: Delete .vscode/settings.json (#16)
    
    * chore: Update build.gradle (#21)
    
    * test: Add tests
    
    * test: Add tests (#24)
    
    * test: Add tests command source (#26)
    
    * test: Добавить тесты Component Style (#27)
    
    * test: Add component style tests
    
    * fix: Тест выявил, что SelectorComponent был изменён в версии 1.17
    
    * chore: Убрать задачи fatJar (#31)
    
    * chore: Update gradle.properties (#12)
    
    * chore: Delete .vscode/settings.json (#16)
    
    * chore: Update build.gradle (#21)
    
    * chore: Убрать задачи fatJar (#30)
    
    * chore: Убрать задачи fatJar
    
    * chore: Заменить sourceSets на tasks.named (#34)
    
    * chore: Fix task compileJava (#36)
    
    * fix: Добавить несовместимость с ChatModerator старой версии (#29)
    
    * chore: Перенос subproject properties в подпроект (#44)
    
    * chore: Update gradle.properties (#12)
    
    * chore: Delete .vscode/settings.json (#16)
    
    * chore: Update build.gradle (#21)
    
    * chore: Убрать задачи fatJar (#30)
    
    * feat: Переопределить метод equals для компонентов (#28)
    
    * fix: Некоторые мельчайшие исправления (#38)
    
    * fix: Некоторые мельчайшие исправления
    
    * refactor: Удалить неправильные конструкторы ChatComponent
    
    * revert: Вернуть truncatedValue в color
    
    * chore: Перенос subproject properties в подпроект (#41)
    
    * chore: Добавить property minecraft_version (#42)
    
    * chore: Добавить fabric properties
    
    * chore: Изменить github workflows

---
+++ fabric/src/main/java/vakiliner/chatcomponentapi/fabric/FabricParser.java
@@ -0,0 +1,239 @@
+package vakiliner.chatcomponentapi.fabric;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import com.mojang.authlib.GameProfile;
+import net.minecraft.ChatFormatting;
+import net.minecraft.Util;
+import net.minecraft.commands.CommandSource;
+import net.minecraft.core.Registry;
+import net.minecraft.network.chat.BaseComponent;
+import net.minecraft.network.chat.ChatType;
+import net.minecraft.network.chat.ClickEvent;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.HoverEvent;
+import net.minecraft.network.chat.SelectorComponent;
+import net.minecraft.network.chat.Style;
+import net.minecraft.network.chat.TextColor;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.scores.PlayerTeam;
+import vakiliner.chatcomponentapi.base.BaseParser;
+import vakiliner.chatcomponentapi.base.ChatCommandSender;
+import vakiliner.chatcomponentapi.base.ChatOfflinePlayer;
+import vakiliner.chatcomponentapi.base.ChatPlayer;
+import vakiliner.chatcomponentapi.base.ChatTeam;
+import vakiliner.chatcomponentapi.common.ChatId;
+import vakiliner.chatcomponentapi.common.ChatMessageType;
+import vakiliner.chatcomponentapi.common.ChatTextColor;
+import vakiliner.chatcomponentapi.common.ChatTextFormat;
+import vakiliner.chatcomponentapi.component.ChatClickEvent;
+import vakiliner.chatcomponentapi.component.ChatComponent;
+import vakiliner.chatcomponentapi.component.ChatComponentFormat;
+import vakiliner.chatcomponentapi.component.ChatComponentWithLegacyText;
+import vakiliner.chatcomponentapi.component.ChatHoverEvent;
+import vakiliner.chatcomponentapi.component.ChatSelectorComponent;
+import vakiliner.chatcomponentapi.component.ChatTextComponent;
+import vakiliner.chatcomponentapi.component.ChatTranslateComponent;
+import vakiliner.chatcomponentapi.fabric.mixin.StyleMixin;
+
+public class FabricParser extends BaseParser {
+	public void sendMessage(CommandSource commandSource, ChatComponent component, ChatMessageType type, UUID uuid) {
+		if (uuid == null) uuid = Util.NIL_UUID;
+		if (commandSource instanceof ServerPlayer) {
+			((ServerPlayer) commandSource).sendMessage(fabric(component), fabric(type), uuid);
+		} else {
+			commandSource.sendMessage(fabric(component), uuid);
+		}
+	}
+
+	public static Component fabric(ChatComponent raw) {
+		final BaseComponent component;
+		if (raw instanceof ChatComponentWithLegacyText) {
+			raw = ((ChatComponentWithLegacyText) raw).getComponent();
+		}
+		if (raw == null) {
+			return null;
+		} else if (raw instanceof ChatTextComponent) {
+			ChatTextComponent chatComponent = (ChatTextComponent) raw;
+			component = new TextComponent(chatComponent.getText());
+		} else if (raw instanceof ChatTranslateComponent) {
+			ChatTranslateComponent chatComponent = (ChatTranslateComponent) raw;
+			component = new TranslatableComponent(chatComponent.getKey(), chatComponent.getWith().stream().map(FabricParser::fabric).toArray());
+		} else if (raw instanceof ChatSelectorComponent) {
+			ChatSelectorComponent chatComponent = (ChatSelectorComponent) raw;
+			component = new SelectorComponent(chatComponent.getSelector());
+		} else {
+			throw new IllegalArgumentException("Could not parse Component from " + raw.getClass());
+		}
+		component.setStyle(fabricStyle(raw));
+		List<ChatComponent> extra = raw.getExtra();
+		if (extra != null) for (ChatComponent chatComponent : extra) {
+			component.append(fabric(chatComponent));
+		}
+		return component;
+	}
+
+	public static ChatComponent fabric(Component raw) {
+		final ChatComponent chatComponent;
+		if (raw == null) {
+			return null;
+		} else if (raw instanceof TextComponent) {
+			TextComponent component = (TextComponent) raw;
+			chatComponent = new ChatTextComponent(component.getText());
+		} else if (raw instanceof TranslatableComponent) {
+			TranslatableComponent component = (TranslatableComponent) raw;
+			chatComponent = new ChatTranslateComponent(null, component.getKey(), Arrays.stream(component.getArgs()).map((arg) -> arg instanceof Component ? fabric((Component) arg) : new ChatTextComponent(String.valueOf(arg))).collect(Collectors.toList()));
+		} else if (raw instanceof SelectorComponent) {
+			SelectorComponent component = (SelectorComponent) raw;
+			chatComponent = new ChatSelectorComponent(component.getPattern());
+		} else {
+			throw new IllegalArgumentException("Could not parse ChatComponent from " + raw.getClass());
+		}
+		Style style = raw.getStyle();
+		chatComponent.setColor(fabric(style.getColor()));
+		chatComponent.setBold(((StyleMixin) style).getBold());
+		chatComponent.setItalic(((StyleMixin) style).getItalic());
+		chatComponent.setStrikethrough(((StyleMixin) style).getStrikethrough());
+		chatComponent.setUnderlined(((StyleMixin) style).getUnderlined());
+		chatComponent.setObfuscated(((StyleMixin) style).getObfuscated());
+		chatComponent.setClickEvent(fabric(style.getClickEvent()));
+		chatComponent.setHoverEvent(fabric(style.getHoverEvent()));
+		chatComponent.setExtra(raw.getSiblings().stream().map(FabricParser::fabric).collect(Collectors.toList()));
+		return chatComponent;
+	}
+
+	public static ClickEvent fabric(ChatClickEvent event) {
+		return event != null ? new ClickEvent(ClickEvent.Action.getByName(event.getAction().getName()), event.getValue()) : null;
+	}
+
+	public static ChatClickEvent fabric(ClickEvent event) {
+		return event != null ? new ChatClickEvent(ChatClickEvent.Action.getByName(event.getAction().getName()), event.getValue()) : null;
+	}
+
+	@SuppressWarnings("unchecked")
+	public static HoverEvent fabric(ChatHoverEvent<?> event) {
+		return event != null ? new HoverEvent(HoverEvent.Action.getByName(event.getAction().getName()), fabricContent(event.getContents())) : null;
+	}
+
+	@SuppressWarnings("unchecked")
+	public static <V> ChatHoverEvent<V> fabric(HoverEvent event) {
+		if (event == null) return null;
+		HoverEvent.Action<?> action = event.getAction();
+		return new ChatHoverEvent<>((ChatHoverEvent.Action<V>) ChatHoverEvent.Action.getByName(action.getName()), (V) fabricContent2(event.getValue(action)));
+	}
+
+	public static ResourceLocation fabric(ChatId id) {
+		return id != null ? new ResourceLocation(id.getNamespace(), id.getValue()) : null;
+	}
+
+	public static ChatId fabric(ResourceLocation resourceLocation) {
+		return resourceLocation != null ? new ChatId(resourceLocation.getNamespace(), resourceLocation.getPath()) : null;
+	}
+
+	public static ChatType fabric(ChatMessageType type) {
+		return type != null ? ChatType.valueOf(type.name()) : null;
+	}
+
+	public static ChatMessageType fabric(ChatType type) {
+		return type != null ? ChatMessageType.valueOf(type.name()) : null;
+	}
+
+	public static Style fabricStyle(ChatComponent component) {
+		Objects.requireNonNull(component);
+		Style style = Style.EMPTY;
+		TextColor color = fabric(component.getColorRaw());
+		if (color != null) {
+			style = style.withColor(color);
+		}
+		for (Map.Entry<ChatComponentFormat, Boolean> entry : component.getFormatsRaw().entrySet()) {
+			Boolean isSetted = entry.getValue();
+			if (isSetted != null && isSetted) {
+				style = style.applyFormat(FabricParser.fabric(entry.getKey().asTextFormat()));
+			}
+		}
+		ChatClickEvent clickEvent = component.getClickEvent();
+		if (clickEvent != null) style = style.withClickEvent(FabricParser.fabric(clickEvent));
+		ChatHoverEvent<?> hoverEvent = component.getHoverEvent();
+		if (hoverEvent != null) style = style.withHoverEvent(FabricParser.fabric(hoverEvent));
+		return style;
+	}
+
+	public static ChatFormatting fabric(ChatTextFormat format) {
+		return format != null ? ChatFormatting.getByCode(format.getChar()) : null;
+	}
+
+	public static ChatTextFormat fabric(ChatFormatting formatting) {
+		return formatting != null ? ChatTextFormat.getByChar(formatting.toString().charAt(1)) : null;
+	}
+
+	public static Object fabricContent(Object raw) {
+		if (raw == null) {
+			return null;
+		} else if (raw instanceof ChatComponent) {
+			ChatComponent content = (ChatComponent) raw;
+			return FabricParser.fabric(content);
+		} else if (raw instanceof ChatHoverEvent.ShowEntity) {
+			ChatHoverEvent.ShowEntity content = (ChatHoverEvent.ShowEntity) raw;
+			return new HoverEvent.EntityTooltipInfo(Registry.ENTITY_TYPE.get(FabricParser.fabric(content.getType())), content.getUniqueId(), FabricParser.fabric(content.getName()));
+		} else if (raw instanceof ChatHoverEvent.ShowItem) {
+			ChatHoverEvent.ShowItem content = (ChatHoverEvent.ShowItem) raw;
+			return new HoverEvent.ItemStackInfo(new ItemStack(Registry.ITEM.get(FabricParser.fabric(content.getItem())), content.getCount()));
+		} else {
+			throw new IllegalArgumentException("Could not parse Content from " + raw.getClass());
+		}
+	}
+
+	public static Object fabricContent2(Object raw) {
+		if (raw == null) {
+			return null;
+		} else if (raw instanceof Component) {
+			Component content = (Component) raw;
+			return FabricParser.fabric(content);
+		} else if (raw instanceof HoverEvent.EntityTooltipInfo) {
+			HoverEvent.EntityTooltipInfo content = (HoverEvent.EntityTooltipInfo) raw;
+			return new ChatHoverEvent.ShowEntity(FabricParser.fabric(Registry.ENTITY_TYPE.getKey(content.type)), content.id, FabricParser.fabric(content.name));
+		} else if (raw instanceof HoverEvent.ItemStackInfo) {
+			HoverEvent.ItemStackInfo content = (HoverEvent.ItemStackInfo) raw;
+			ItemStack itemStack = content.getItemStack();
+			return new ChatHoverEvent.ShowItem(FabricParser.fabric(Registry.ITEM.getKey(itemStack.getItem())), itemStack.getCount());
+		} else {
+			throw new IllegalArgumentException("Could not parse ChatContent from " + raw.getClass());
+		}
+	}
+
+	public static TextColor fabric(ChatTextColor color) {
+		return color != null ? TextColor.fromRgb(color.value()) : null;
+	}
+
+	public static ChatTextColor fabric(TextColor color) {
+		return color != null ? ChatTextColor.color(color.getValue(), null) : null;
+	}
+
+	public ChatPlayer toChatPlayer(ServerPlayer player) {
+		return player != null ? new FabricChatPlayer(this, player) : null;
+	}
+
+	public ChatOfflinePlayer toChatOfflinePlayer(MinecraftServer server, GameProfile gameProfile) {
+		return gameProfile != null ? new FabricChatOfflinePlayer(this, server, gameProfile) : null;
+	}
+
+	public ChatCommandSender toChatCommandSender(CommandSource commandSource) {
+		if (commandSource instanceof ServerPlayer) {
+			return this.toChatPlayer((ServerPlayer) commandSource);
+		}
+		return commandSource != null ? new FabricChatCommandSender(this, commandSource) : null;
+	}
+
+	public ChatTeam toChatTeam(PlayerTeam team) {
+		return team != null ? new FabricChatTeam(this, team) : null;
+	}
+}
