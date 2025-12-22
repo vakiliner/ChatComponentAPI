62520e963cf0b97c0406c3d08a0734387ec01eb1
VAKiliner <119078586+vakiliner@users.noreply.github.com>
2025-12-22 17:15:19 +0300

    chore: Объединить подпроект в одну ветку (#48)
    * chore: Добавить Forge
    
    * chore: Update settings.gradle (#9)
    
    * chore: Update gradle.properties (#11)
    
    * chore: Изменить стиль dependencies в build.gradle (#15)
    
    * chore: Delete .vscode/settings.json (#18)
    
    * test: Add tests (#25)
    
    * chore: Update gradle.properties (#12)
    
    * chore: Delete .vscode/settings.json (#16)
    
    * chore: Update build.gradle (#21)
    
    * test: Add tests
    
    * chore: Remove plugins
    
    * fix: Mixin compatibilityLevel
    
    * fix: Delete onInitialize
    
    * test: Add test count
    
    * chore: Убрать задачи fatJar (#32)
    
    * chore: Update gradle.properties (#12)
    
    * chore: Delete .vscode/settings.json (#16)
    
    * chore: Update build.gradle (#21)
    
    * chore: Убрать задачи fatJar (#30)
    
    * chore: Убрать задачи fatJar
    
    * chore: Заменить sourceSets на tasks.named (#35)
    
    * chore: Fix task compileJava (#37)
    
    * chore: Перенос subproject properties в подпроект (#45)
    
    * chore: Update gradle.properties (#12)
    
    * chore: Delete .vscode/settings.json (#16)
    
    * chore: Update build.gradle (#21)
    
    * chore: Убрать задачи fatJar (#30)
    
    * feat: Переопределить метод equals для компонентов (#28)
    
    * fix: Некоторые мельчайшие исправления (#38)
    
    * fix: Некоторые мельчайшие исправления
    
    * refactor: Удалить неправильные конструкторы ChatComponent
    
    * revert: Вернуть truncatedValue в color
    
    * chore: Перенос subproject properties в подпроект (#41)
    
    * chore: Добавить property minecraft_version (#42)
    
    * chore: Добавить forge properties
    
    * chore: Изменить github workflows

---
+++ forge/src/main/java/vakiliner/chatcomponentapi/ChatComponentAPIForgeLoader.java
@@ -0,0 +1,9 @@
+package vakiliner.chatcomponentapi;
+
+import net.minecraftforge.fml.common.Mod;
+import vakiliner.chatcomponentapi.forge.ForgeParser;
+
+@Mod("chatcomponentapi")
+public class ChatComponentAPIForgeLoader {
+	public static final ForgeParser PARSER = new ForgeParser();
+}
