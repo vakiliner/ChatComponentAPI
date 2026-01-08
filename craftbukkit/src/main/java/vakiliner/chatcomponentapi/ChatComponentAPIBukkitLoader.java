3d792ebe095ed6f36200d2b404014a6baa85b4cc
VAKiliner <119078586+vakiliner@users.noreply.github.com>
2026-01-08 15:59:00 +0300

    chore: Перенос кода из compileJava и processResources в таск jar (#65)
    * chore: Улучшить fat jar, перенос кода из compileJava и processResources в таск jar
    
    * chore: Учесть manifest
    
    * refactor: Без локального переменного
    
    Signed-off-by: VAKiliner <119078586+vakiliner@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: VAKiliner <119078586+vakiliner@users.noreply.github.com>

---
+++ craftbukkit/src/main/java/vakiliner/chatcomponentapi/ChatComponentAPIBukkitLoader.java
@@ -0,0 +1,12 @@
+package vakiliner.chatcomponentapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import vakiliner.chatcomponentapi.craftbukkit.BukkitParser;
+
+public class ChatComponentAPIBukkitLoader extends JavaPlugin {
+	public static final BukkitParser PARSER;
+
+	static {
+		PARSER = new BukkitParser();
+	}
+}
