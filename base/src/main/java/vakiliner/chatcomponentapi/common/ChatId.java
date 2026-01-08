bc0de1283e3151a9b8796b23d5e9f235c97de002
VAKiliner <119078586+vakiliner@users.noreply.github.com>
2026-01-08 22:10:12 +0300

    feat: Add ChatId.parse (#73)

--- base/src/main/java/vakiliner/chatcomponentapi/common/ChatId.java
+++ base/src/main/java/vakiliner/chatcomponentapi/common/ChatId.java
@@ -6,12 +6,6 @@ public class ChatId {
 	private final String namespace;
 	private final String value;
 
-	public ChatId(String string) {
-		int index = string.indexOf(':');
-		this.namespace = index > 0 ? string.substring(0, index) : "minecraft";
-		this.value = index >= 0 ? string.substring(index + 1) : string;
-	}
-
 	public ChatId(String namespace, String value) {
 		this.namespace = Objects.requireNonNull(namespace);
 		this.value = Objects.requireNonNull(value);
@@ -38,5 +32,16 @@ public class ChatId {
 
 	public String toString() {
 		return this.namespace + ':' + this.value;
+	}
+
+	public static ChatId parse(String string) {
+		return parse(string, "minecraft");
+	}
+
+	public static ChatId parse(String string, String defaultNamespace) {
+		int index = string.indexOf(':');
+		String namespace = index > 0 ? string.substring(0, index) : defaultNamespace;
+		String value = index >= 0 ? string.substring(index + 1) : string;
+		return new ChatId(namespace, value);
 	}
 }
