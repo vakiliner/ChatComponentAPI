cad8d679d9ab98f9c01cd38d492bf14b4dd05d25
VAKiliner <119078586+vakiliner@users.noreply.github.com>
2026-01-11 22:38:05 +0300

    chore: BukkitLoader

---
+++ bukkit/src/main/java/vakiliner/chatcomponentapi/craftbukkit/BukkitParser.java
@@ -0,0 +1,9 @@
+package vakiliner.chatcomponentapi.craftbukkit;
+
+import vakiliner.chatcomponentapi.base.BaseParser;
+
+public class BukkitParser extends BaseParser {
+	public boolean supportsSeparatorInSelector() {
+		throw new UnsupportedOperationException();
+	}
+}
