5a386e4eed083736f271be4731177c4e9882e2ec
VAKiliner <119078586+vakiliner@users.noreply.github.com>
2026-01-20 23:34:09 +0300

    refactor: Examples IChatPlugin

--- forge/src/main/java/vakiliner/chatcomponentapi/ChatComponentAPIForgeLoader.java
+++ forge/src/main/java/vakiliner/chatcomponentapi/ChatComponentAPIForgeLoader.java
@@ -1,9 +1,10 @@
 package vakiliner.chatcomponentapi;
 
 import net.minecraftforge.fml.common.Mod;
+import vakiliner.chatcomponentapi.base.IChatPlugin;
 import vakiliner.chatcomponentapi.forge.ForgeParser;
 
 @Mod("chatcomponentapi")
-public class ChatComponentAPIForgeLoader {
+public class ChatComponentAPIForgeLoader implements IChatPlugin {
 	public static final ForgeParser PARSER = new ForgeParser();
 }
