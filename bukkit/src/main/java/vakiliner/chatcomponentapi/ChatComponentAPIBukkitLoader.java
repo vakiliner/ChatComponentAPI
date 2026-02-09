0c2a8e27ec98ef87ff8617b4262fda9e7cd6b998
VAKiliner <119078586+vakiliner@users.noreply.github.com>
2026-02-09 03:29:35 +0300

    refactor: Усовершенствовать IChatPlugin (#108)
    * refactor: Усовершенствовать IChatPlugin
    
    * chore: Fix circular dependency

--- bukkit/src/main/java/vakiliner/chatcomponentapi/ChatComponentAPIBukkitLoader.java
+++ bukkit/src/main/java/vakiliner/chatcomponentapi/ChatComponentAPIBukkitLoader.java
@@ -1,12 +1,13 @@
 package vakiliner.chatcomponentapi;
 
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
-import vakiliner.chatcomponentapi.base.IChatPlugin;
 import vakiliner.chatcomponentapi.craftbukkit.BukkitParser;
+import vakiliner.chatcomponentapi.craftbukkit.IBukkitChatPlugin;
 import vakiliner.chatcomponentapi.paper.PaperParser;
 import vakiliner.chatcomponentapi.spigot.SpigotParser;
 
-public class ChatComponentAPIBukkitLoader extends JavaPlugin implements IChatPlugin {
+public class ChatComponentAPIBukkitLoader extends JavaPlugin implements IBukkitChatPlugin {
 	public static final BukkitParser PARSER;
 
 	static {
@@ -21,5 +22,9 @@ public class ChatComponentAPIBukkitLoader extends JavaPlugin implements IChatPlu
 			}
 		}
 		PARSER = impl;
+	}
+
+	public Plugin asPlugin() {
+		return this;
 	}
 }
