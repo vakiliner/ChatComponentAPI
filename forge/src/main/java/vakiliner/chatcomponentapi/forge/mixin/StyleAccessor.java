2a8937bb7af8991275b7b505b08d57b479ad6c91
VAKiliner <119078586+vakiliner@users.noreply.github.com>
2026-08-08 19:02:01 +0300

    feat: StyleAccessor.toChatComponentAPI()

--- forge/src/main/java/vakiliner/chatcomponentapi/forge/mixin/StyleAccessor.java
+++ forge/src/main/java/vakiliner/chatcomponentapi/forge/mixin/StyleAccessor.java
@@ -8,12 +8,29 @@ import net.minecraft.util.text.Color;
 import net.minecraft.util.text.Style;
 import net.minecraft.util.text.event.ClickEvent;
 import net.minecraft.util.text.event.HoverEvent;
+import vakiliner.chatcomponentapi.component.ChatStyle;
+import vakiliner.chatcomponentapi.forge.ForgeParser;
 
 @Mixin(Style.class)
 public interface StyleAccessor {
 	@Invoker("<init>")
 	static Style newStyle(Color сolor, Boolean bold, Boolean italic, Boolean underlined, Boolean strikethrough, Boolean obfuscated, ClickEvent clickEvent, HoverEvent hoverEvent, String insertion, ResourceLocation font) {
 		throw new AssertionError();
+	}
+
+	default ChatStyle toChatComponentAPI() {
+		ChatStyle.Builder builder = ChatStyle.newBuilder();
+		builder.withColor(ForgeParser.forge(this.getColor()));
+		builder.withBold(this.getBold());
+		builder.withItalic(this.getItalic());
+		builder.withUnderlined(this.getUnderlined());
+		builder.withStrikethrough(this.getStrikethrough());
+		builder.withObfuscated(this.getObfuscated());
+		builder.withClickEvent(ForgeParser.forge(this.getClickEvent()));
+		builder.withHoverEvent(ForgeParser.forge(this.getHoverEvent()));
+		builder.withInsertion(this.getInsertion());
+		builder.withFont(ForgeParser.forge(this.$getFont()));
+		return builder.build();
 	}
 
 	@Accessor("color")
