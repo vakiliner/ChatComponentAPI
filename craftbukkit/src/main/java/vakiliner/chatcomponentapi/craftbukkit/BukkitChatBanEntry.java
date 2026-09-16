99c1c90bba4ec13df4fa407149f212fdaaa86b5a
VAKiliner <119078586+vakiliner@users.noreply.github.com>
2026-09-16 08:08:48 +0300

    fix: Merge fixes for Bukkit & other fixes

--- craftbukkit/src/main/java/vakiliner/chatcomponentapi/craftbukkit/BukkitChatBanEntry.java
+++ craftbukkit/src/main/java/vakiliner/chatcomponentapi/craftbukkit/BukkitChatBanEntry.java
@@ -1,6 +1,7 @@
 package vakiliner.chatcomponentapi.craftbukkit;
 
 import java.util.Date;
+import java.util.Objects;
 import org.bukkit.BanEntry;
 import vakiliner.chatcomponentapi.base.ChatBanEntry;
 
@@ -9,8 +10,8 @@ public class BukkitChatBanEntry implements ChatBanEntry {
 	protected final BanEntry banEntry;
 
 	public BukkitChatBanEntry(BukkitParser parser, BanEntry banEntry) {
-		this.parser = parser;
-		this.banEntry = banEntry;
+		this.parser = Objects.requireNonNull(parser);
+		this.banEntry = Objects.requireNonNull(banEntry);
 	}
 
 	public BanEntry getImpl() {
