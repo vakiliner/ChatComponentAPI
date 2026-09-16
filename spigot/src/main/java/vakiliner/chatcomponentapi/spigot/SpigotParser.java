809ed73ddac3b20f09d8b51fcab920efd2bcc7fa
VAKiliner <119078586+vakiliner@users.noreply.github.com>
2026-09-16 15:02:39 +0300

    chore: Update code to minecraft 1.16.2

--- spigot/src/main/java/vakiliner/chatcomponentapi/spigot/SpigotParser.java
+++ spigot/src/main/java/vakiliner/chatcomponentapi/spigot/SpigotParser.java
@@ -41,17 +41,9 @@ public class SpigotParser extends BukkitParser {
 	private void sendMessage(CommandSender sender, BaseComponent component, ChatMessageType type, UUID uuid) {
 		if (sender instanceof Player) {
 			Player player = (Player) sender;
-			if (uuid != null) {
-				player.spigot().sendMessage(type, uuid, component);
-			} else {
-				player.spigot().sendMessage(type, component);
-			}
+			player.spigot().sendMessage(type, component);
 		} else {
-			if (uuid != null) {
-				sender.spigot().sendMessage(uuid, component);
-			} else {
-				sender.spigot().sendMessage(component);
-			}
+			sender.spigot().sendMessage(component);
 		}
 	}
 
